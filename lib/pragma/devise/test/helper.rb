9443fce72a802e7c074dd03d5ebaea7aff67d9ff
Alessandro Desantis <desa.alessandro@gmail.com>
2017-01-01 22:33:21 +0100

    Add missing documentation

--- lib/pragma/devise/test/helper.rb
+++ lib/pragma/devise/test/helper.rb
@@ -1,12 +1,19 @@
 # frozen_string_literal: true
 module Pragma
   module Test
+    # Provides RSpec helpers for authenticating in tests.
+    #
+    # @author Alessandro Desantis
     module Helper
       def self.included(klass)
         klass.extend ClassMethods
       end
 
-      module ClassMethods
+      module ClassMethods # :nodoc:
+        # Authenticate as the given user by setting the appropriate JWT in the +Authorization+
+        # header.
+        #
+        # @param user [Object] user to authenticate with
         def authenticate_as(user)
           before do
             token = Knock::AuthToken.new(payload: { sub: user.id }).token
