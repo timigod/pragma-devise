fc4f363ce00481d93cdcf3050c416349cff0bf7f
Alessandro Desantis <desa.alessandro@gmail.com>
2017-01-03 18:02:17 +0100

    Fix URL builder for users index

--- app/resources/pragma/devise/user/operation/index.rb
+++ app/resources/pragma/devise/user/operation/index.rb
@@ -9,6 +9,15 @@ module Pragma
           include Pragma::Devise::User::Operation::Defaults
 
           before :authenticate_user
+
+          protected
+
+          def build_page_url(page)
+            Pragma::Devise::Engine.routes.url_helpers.url_for(params.merge(
+              page_param => page,
+              only_path: true
+            ).symbolize_keys)
+          end
         end
       end
     end
